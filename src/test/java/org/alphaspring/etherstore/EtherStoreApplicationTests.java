8815135a33ae95e0c0e11ab770330feeed254d90
samaunmahmud <samaunmahmud9@gmail.com>
2026-07-30 22:17:19 +0100

    Understanding Spring boot debuging system

--- src/test/java/org/alphaspring/etherstore/EtherStoreApplicationTests.java
+++ src/test/java/org/alphaspring/etherstore/EtherStoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EtherStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
