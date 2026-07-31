8aa383f2d395bfc1a66d136c41cced4c371b90bd
samaunmahmud <samaunmahmud9@gmail.com>
2026-07-31 01:13:45 +0100

    Learning @Autowired this is for the classes which have multiple constractor, if using one constructor you do not need to use it, it finds the one needed

--- src/main/java/org/alphaspring/etherstore/OrderService.java
+++ src/main/java/org/alphaspring/etherstore/OrderService.java
@@ -1,11 +1,18 @@
 package org.alphaspring.etherstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     private PaymentService paymentService;
+
+    public OrderService(){
+
+    }
+
+    @Autowired
 
     public OrderService(PaymentService paymentService){
         this.paymentService = paymentService;
